d3f0aa6623c49bd04bf7d8913d3f22e7cb7c6b39
Alex Batishchev <alexbat@microsoft.com>
2016-09-19 15:55:23 -0700

    Fixing package.cmd

--- package.cmd
+++ package.cmd
@@ -5,6 +5,7 @@ if not exist package mkdir package
 if not exist package\lib mkdir package\lib
 if not exist package\lib\3.5 mkdir package\lib\3.5
 
-copy JWT\bin\Release\JWT.dll package\lib\3.5\
+msbuild src\JWT.sln -p:Configuration=Release
+copy src\JWT\bin\Release\JWT.dll package\lib\3.5\
 
 tools\nuget.exe pack JWT.nuspec -BasePath package
