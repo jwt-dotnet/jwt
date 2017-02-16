e72202de6ef49b01f4e7fb77c499fc992203d4b3
Alex Batishchev <alexbat@microsoft.com>
2017-02-16 12:28:58 -0800

    Removing reference to System.Web.Extensions, DefaultJsonSerializer. Adding Json.Net, JsonNetSerializer. Updating nuget packages for Tests

---
+++ src/JWT/JsonNetSerializer.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace JWT
+{
+    public class JsonNetSerializer : IJsonSerializer
+    {
+        public string Serialize(object obj)
+        {
+            return JObject.FromObject(obj).ToString();
+        }
+
+        public T Deserialize<T>(string json)
+        {
+            return JObject.Parse(json).ToObject<T>();
+        }
+    }
+}
