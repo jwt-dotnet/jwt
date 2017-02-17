1d31af70333c21361bf42038351a6077f1d47093
Alex Batishchev <alexbat@microsoft.com>
2017-02-17 13:44:43 -0800

    Updating nuspec, package.cmd, nuget.exe

--- package.cmd
+++ package.cmd
@@ -3,9 +3,9 @@
 if not exist package mkdir package
 if not exist package mkdir package
 if not exist package\lib mkdir package\lib
-if not exist package\lib\3.5 mkdir package\lib\3.5
+if not exist package\lib\net45 mkdir package\lib\net45
 
 msbuild src\JWT.sln -p:Configuration=Release
-copy src\JWT\bin\Release\JWT.dll package\lib\3.5\
+copy src\JWT\bin\Release\JWT.dll package\lib\net45
 
 tools\nuget.exe pack JWT.nuspec -BasePath package
