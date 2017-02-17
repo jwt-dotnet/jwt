4aa70df10575c0c33a7a1365631c60bbc78a9046
Alex Batishchev <alexbat@microsoft.com>
2017-02-17 11:52:41 -0800

    Adding JwtDecoder, moving Decode() methods to it

--- src/JWT/IJwtDecoder.cs
+++ src/JWT/IJwtDecoder.cs
@@ -2,5 +2,72 @@
 {
     public interface IJwtDecoder
     {
+        /// <summary>
+        /// Given a JWT, decode it and return the JSON payload.
+        /// </summary>
+        /// <param name="token">The JWT.</param>
+        /// <param name="key">The key that was used to sign the JWT.</param>
+        /// <param name="verify">Whether to verify the signature (default is true).</param>
+        /// <returns>A string containing the JSON payload.</returns>
+        /// <exception cref="SignatureVerificationException">Thrown if the verify parameter was true and the signature was NOT valid or if the JWT was signed with an unsupported algorithm.</exception>
+        /// <exception cref="TokenExpiredException">Thrown if the verify parameter was true and the token has an expired exp claim.</exception>
+        string Decode(string token, string key, bool verify = true);
+
+        /// <summary>
+        /// Given a JWT, decode it and return the JSON payload.
+        /// </summary>
+        /// <param name="token">The JWT.</param>
+        /// <param name="key">The key bytes that were used to sign the JWT.</param>
+        /// <param name="verify">Whether to verify the signature (default is true).</param>
+        /// <returns>A string containing the JSON payload.</returns>
+        /// <exception cref="SignatureVerificationException">Thrown if the verify parameter was true and the signature was NOT valid or if the JWT was signed with an unsupported algorithm.</exception>
+        /// <exception cref="TokenExpiredException">Thrown if the verify parameter was true and the token has an expired exp claim.</exception>
+        string Decode(string token, byte[] key, bool verify = true);
+
+        /// <summary>
+        /// Given a JWT, decode it and return the payload as an object.
+        /// </summary>
+        /// <param name="token">The JWT.</param>
+        /// <param name="key">The key that was used to sign the JWT.</param>
+        /// <param name="verify">Whether to verify the signature (default is true).</param>
+        /// <returns>An object representing the payload.</returns>
+        /// <exception cref="SignatureVerificationException">Thrown if the verify parameter was true and the signature was NOT valid or if the JWT was signed with an unsupported algorithm.</exception>
+        /// <exception cref="TokenExpiredException">Thrown if the verify parameter was true and the token has an expired exp claim.</exception>
+        object DecodeToObject(string token, string key, bool verify = true);
+
+        /// <summary>
+        /// Given a JWT, decode it and return the payload as an object.
+        /// </summary>
+        /// <param name="token">The JWT.</param>
+        /// <param name="key">The key that was used to sign the JWT.</param>
+        /// <param name="verify">Whether to verify the signature (default is true).</param>
+        /// <returns>An object representing the payload.</returns>
+        /// <exception cref="SignatureVerificationException">Thrown if the verify parameter was true and the signature was NOT valid or if the JWT was signed with an unsupported algorithm.</exception>
+        /// <exception cref="TokenExpiredException">Thrown if the verify parameter was true and the token has an expired exp claim.</exception>
+        object DecodeToObject(string token, byte[] key, bool verify = true);
+
+        /// <summary>
+        /// Given a JWT, decode it and return the payload as an object.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="Type"/> to return</typeparam>
+        /// <param name="token">The JWT.</param>
+        /// <param name="key">The key that was used to sign the JWT.</param>
+        /// <param name="verify">Whether to verify the signature (default is true).</param>
+        /// <returns>An object representing the payload.</returns>
+        /// <exception cref="SignatureVerificationException">Thrown if the verify parameter was true and the signature was NOT valid or if the JWT was signed with an unsupported algorithm.</exception>
+        /// <exception cref="TokenExpiredException">Thrown if the verify parameter was true and the token has an expired exp claim.</exception>
+        T DecodeToObject<T>(string token, string key, bool verify = true);
+
+        /// <summary>
+        /// Given a JWT, decode it and return the payload as an object.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="Type"/> to return</typeparam>
+        /// <param name="token">The JWT.</param>
+        /// <param name="key">The key that was used to sign the JWT.</param>
+        /// <param name="verify">Whether to verify the signature (default is true).</param>
+        /// <returns>An object representing the payload.</returns>
+        /// <exception cref="SignatureVerificationException">Thrown if the verify parameter was true and the signature was NOT valid or if the JWT was signed with an unsupported algorithm.</exception>
+        /// <exception cref="TokenExpiredException">Thrown if the verify parameter was true and the token has an expired exp claim.</exception>
+        T DecodeToObject<T>(string token, byte[] key, bool verify = true);
     }
 }
---
+++ src/JWT/JwtDecoder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JWT
+{
+    public class JwtDecoder : IJwtDecoder
+    {
+        /// <inheritdoc />
+        public string Decode(string token, string key, bool verify = true)
+        {
+            return Decode(token, Encoding.UTF8.GetBytes(key), verify);
+        }
+
+        /// <inheritdoc />
+        public string Decode(string token, byte[] key, bool verify = true)
+        {
+            var parts = token.Split('.');
+            if (parts.Length != 3)
+            {
+                throw new ArgumentException("Token must consist from 3 delimited by dot parts");
+            }
+
+            var payload = parts[1];
+            var payloadJson = Encoding.UTF8.GetString(Base64UrlDecode(payload));
+
+            if (verify)
+            {
+                Verify(payload, payloadJson, parts, key);
+            }
+
+            return payloadJson;
+        }
+
+        /// <inheritdoc />
+        public object DecodeToObject(string token, string key, bool verify = true)
+        {
+            return DecodeToObject(token, Encoding.UTF8.GetBytes(key), verify);
+        }
+
+        /// <inheritdoc />
+        public object DecodeToObject(string token, byte[] key, bool verify = true)
+        {
+            var payloadJson = Decode(token, key, verify);
+            return JsonSerializer.Deserialize<Dictionary<string, object>>(payloadJson);
+        }
+
+        /// <inheritdoc />
+        public T DecodeToObject<T>(string token, string key, bool verify = true)
+        {
+            return DecodeToObject<T>(token, Encoding.UTF8.GetBytes(key), verify);
+        }
+
+        /// <inheritdoc />
+        public T DecodeToObject<T>(string token, byte[] key, bool verify = true)
+        {
+            var payloadJson = Decode(token, key, verify);
+            return JsonSerializer.Deserialize<T>(payloadJson);
+        }
+    }
+}
