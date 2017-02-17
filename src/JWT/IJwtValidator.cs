2a85c834a772b8bdb30a408c07ef842785c82ef5
Alex Batishchev <alexbat@microsoft.com>
2017-02-17 13:18:05 -0800

    Moving validation to JwtValidator

--- src/JWT/IJwtValidator.cs
+++ src/JWT/IJwtValidator.cs
@@ -1,6 +1,15 @@
 ﻿namespace JWT
 {
-    interface IJwtValidator
+    public interface IJwtValidator
     {
+        /// <summary>
+        /// Given the JWT, verifies it.
+        /// </summary>
+        /// <param name="payloadJson">>An arbitrary payload (already serialized to JSON).</param>
+        /// <param name="decodedCrypto">Decoded body</param>
+        /// <param name="decodedSignature">Decoded signature</param>
+        /// <exception cref="SignatureVerificationException">The signature is invalid.</exception>
+        /// <exception cref="TokenExpiredException">The token has expired.</exception>
+        void Validate(string payloadJson, string decodedCrypto, string decodedSignature);
     }
 }
---
+++ src/JWT/JwtValidator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JWT
+{
+    public sealed class JwtValidator : IJwtValidator
+    {
+        private static readonly DateTime _unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+        private readonly IJsonSerializer _jsonSerializer;
+
+        public JwtValidator(IJsonSerializer jsonSerializer)
+        {
+            _jsonSerializer = jsonSerializer;
+        }
+
+        /// <inheritdoc />
+        public void Validate(string payloadJson, string decodedCrypto, string decodedSignature)
+        {
+            if (decodedCrypto != decodedSignature)
+            {
+                throw new SignatureVerificationException("Invalid signature")
+                {
+                    Expected = decodedCrypto,
+                    Received = decodedSignature
+                };
+            }
+
+            // verify exp claim https://tools.ietf.org/html/draft-ietf-oauth-json-web-token-32#section-4.1.4
+            var payloadData = _jsonSerializer.Deserialize<Dictionary<string, object>>(payloadJson);
+            object expObj;
+            if (!payloadData.TryGetValue("exp", out expObj) || expObj == null)
+            {
+                return;
+            }
+            int expInt;
+            try
+            {
+                expInt = Convert.ToInt32(expObj);
+            }
+            catch (FormatException)
+            {
+                throw new SignatureVerificationException("Claim 'exp' must be an integer.");
+            }
+            var secondsSinceEpoch = Math.Round((DateTime.UtcNow - _unixEpoch).TotalSeconds);
+            if (secondsSinceEpoch >= expInt)
+            {
+                throw new TokenExpiredException("Token has expired.")
+                {
+                    Expiration = _unixEpoch.AddSeconds(expInt),
+                    PayloadData = payloadData
+                };
+            }
+        }
+    }
+}
