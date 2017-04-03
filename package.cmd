cc0608487d7346b476de67d23068fa1e4b0b9b40
Alex Batishchev <alexbat@microsoft.com>
2017-04-03 15:31:18 -0700

    Updating build script

--- package.cmd
+++ package.cmd
@@ -3,9 +3,9 @@
 if not exist package mkdir package
 if not exist package mkdir package
 if not exist package\lib mkdir package\lib
-if not exist package\lib\net45 mkdir package\lib\net45
+if not exist package\lib\net35 mkdir package\lib\net35
 
 msbuild src\JWT.sln -p:Configuration=Release
-copy src\JWT\bin\Release\JWT.dll package\lib\net45
+copy src\JWT\bin\Release\JWT.dll package\lib\net35
 
 tools\nuget.exe pack JWT.nuspec -BasePath package
