b6cc9669e6b0a702d861af3896651fd68c7ee0e7
Alex Batishchev <alexbat@microsoft.com>
2017-04-03 15:10:48 -0700

    Updating tests

--- tests/JWT.Tests/JwtEncoderTest.cs
+++ tests/JWT.Tests/JwtEncoderTest.cs
@@ -17,7 +17,8 @@ namespace JWT.Tests
         public void Encode_Should_Encode_To_Token()
         {
             var serializer = new JsonNetSerializer();
-            var encoder = new JwtEncoder(new HMACSHA256Algorithm(), serializer);
+            var urlEncoder = new JwtBase64UrlEncoder();
+            var encoder = new JwtEncoder(new HMACSHA256Algorithm(), serializer, urlEncoder);
 
             var actual = encoder.Encode(_customer, "ABC");
 
@@ -28,7 +29,8 @@ namespace JWT.Tests
         public void Encode_Should_Encode_To_Token_With_Extra_Headers()
         {
             var serializer = new JsonNetSerializer();
-            var encoder = new JwtEncoder(new HMACSHA256Algorithm(), serializer);
+            var urlEncoder = new JwtBase64UrlEncoder();
+            var encoder = new JwtEncoder(new HMACSHA256Algorithm(), serializer, urlEncoder);
 
             var extraheaders = new Dictionary<string, object> { { "foo", "bar" } };
             var actual = encoder.Encode(extraheaders, _customer, "ABC");
