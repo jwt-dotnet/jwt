f68b85fabbff854a3b96904112d672a017f77de6
Alexander Batishchev <abatishchev@gmail.com>
2017-04-07 09:22:55 -0700

    Rollingback package.cmd

--- package.cmd
+++ package.cmd
@@ -3,9 +3,9 @@
 if not exist package mkdir package
 if not exist package mkdir package
 if not exist package\lib mkdir package\lib
-if not exist package\lib\net35 mkdir package\lib\net35
+if not exist package\lib\net45 mkdir package\lib\net45
 
 msbuild src\JWT.sln -p:Configuration=Release
-copy src\JWT\bin\Release\JWT.dll package\lib\net35
+copy src\JWT\bin\Release\JWT.dll package\lib\net45
 
 tools\nuget.exe pack JWT.nuspec -BasePath package
