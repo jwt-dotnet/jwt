7d4be5bd8fb0aba8b51fa39a3029500283a7e5f7
Alex Batishchev <alexbat@microsoft.com>
2017-05-01 10:57:23 -0700

    Moving IAlgorithmFactory to the Algorithms namespace/folder

---
+++ src/JWT/Algorithms/IAlgorithmFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace JWT.Algorithms
+{
+    /// <summary>
+    /// Provides IJwtAlgorithms.
+    /// </summary>
+    public interface IAlgorithmFactory
+    {
+        /// <summary>
+        /// Creates an AlgorithmFactory using the provided algorithm name.
+        /// </summary>
+        /// <param name="algorithmName">The name of the algorithm.</param>
+        IJwtAlgorithm Create(string algorithmName);
+
+        /// <summary>
+        /// Creates an AlgorithmFactory using the provided algorithm enum.
+        /// </summary>
+        /// <param name="algorithm">The enum value of the algorithm.</param>
+        IJwtAlgorithm Create(JwtHashAlgorithm algorithm);
+    }
+}
--- src/JWT/IAlgorithmFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JWT
-{
-    public interface IAlgorithmFactory
-    {
-        IJwtAlgorithm Create(string algorithmName);
-
-        IJwtAlgorithm Create(JwtHashAlgorithm algorithm);
-    }
-}
