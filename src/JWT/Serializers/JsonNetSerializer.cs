f5aed9c988f30c47232adc036215246bb85ecc96
Alex Batishchev <alexbat@microsoft.com>
2017-06-23 11:55:26 -0700

    Adding to JsonNetSerializer ctor accepting JsonSerializer

--- src/JWT/Serializers/JsonNetSerializer.cs
+++ src/JWT/Serializers/JsonNetSerializer.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace JWT.Serializers
@@ -8,6 +9,27 @@ namespace JWT.Serializers
     /// </summary>
     public sealed class JsonNetSerializer : IJsonSerializer
     {
+        private readonly JsonSerializer _serializer;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="JsonNetSerializer" />.
+        /// </summary>
+        /// <remarks>Uses <see cref="JsonSerializer.CreateDefault()" /> as internal serializer.</remarks>
+        public JsonNetSerializer()
+            : this(JsonSerializer.CreateDefault())
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="JsonNetSerializer" />.
+        /// </summary>
+        /// <param name="serializer">Internal <see cref="JsonSerializer" /> to use for serialization.</param>
+        public JsonNetSerializer(JsonSerializer serializer)
+        {
+            _serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
+        }
+
         /// <summary>
         /// Serialize the given object.
         /// </summary>
@@ -15,7 +37,7 @@ namespace JWT.Serializers
         /// <returns></returns>
         public string Serialize(object obj)
         {
-            return JObject.FromObject(obj).ToString(Formatting.None);
+            return JObject.FromObject(obj, _serializer).ToString(Formatting.None);
         }
 
         /// <summary>
