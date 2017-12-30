474aa4dd8abc6ab2c0e6d35a296cca5197572dff
Alex Batishchev <alexbat@microsoft.com>
2017-12-29 22:04:24 -0800

    Moving IAlg to Algs

---
+++ src/JWT/Algorithms/IJwtAlgorithm.cs
@@ -0,0 +1,20 @@
+namespace JWT.Algorithms
+{
+    /// <summary>
+    /// Represents an algorithm to generate JWT signature.
+    /// </summary>
+    public interface IJwtAlgorithm
+    {
+        /// <summary>
+        /// Signs provided byte array with provided key.
+        /// </summary>
+        /// <param name="key">The key used to sign the data.</param>
+        /// <param name="bytesToSign">The data to sign.</param>
+        byte[] Sign(byte[] key, byte[] bytesToSign);
+
+        /// <summary>
+        /// The algorithm name.
+        /// </summary>
+        string Name { get; }
+    }
+}
--- src/JWT/JwtEncoder.cs
+++ src/JWT/JwtEncoder.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using JWT.Algorithms;
 
 namespace JWT
 {
