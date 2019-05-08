02bb69d6e2ac6d4a4c4c624df79656b6d15af29d
Alexander Batishchev <abatishchev@gmail.com>
2019-05-08 10:50:18 -0700

    Undo

--- src/JWT/Algorithms/RS256Algorithm.cs
+++ src/JWT/Algorithms/RS256Algorithm.cs
@@ -8,32 +8,48 @@ namespace JWT.Algorithms
     /// </summary>
     public sealed class RS256Algorithm : IJwtAlgorithm
     {
-        private readonly X509Certificate2 _cert;
+        private readonly RSACryptoServiceProvider _publicKey;
+        private readonly RSA _privateKey;
+
+        /// <summary>
+        /// Creates an instance using the provided pair of public and private keys.
+        /// </summary>
+        /// <param name="publicKey">The RSA service provider for verifying the data.</param>
+        /// <param name="privateKey">The RSA key for signing the data.</param>
+        public RS256Algorithm(RSACryptoServiceProvider publicKey, RSA privateKey)
+        {
+            _publicKey = publicKey;
+            _privateKey = privateKey;
+        }
 
         /// <summary>
         /// Creates an instance using the provided certificate.
         /// </summary>
-        public RS256Algorithm(X509Certificate2 cert) =>
-            _cert = cert;
+        /// <param name="cert">The certificate having both public and private keys.</param>
+        public RS256Algorithm(X509Certificate2 cert)
+            : this(GetPublicKey(cert), GetPrivateKey(cert))
+        {
+        }
 
         /// <inheritdoc />
-        public string Name { get; } = JwtHashAlgorithm.RS256.ToString();
+        public string Name =>
+            JwtHashAlgorithm.RS256.ToString();
 
         /// <inheritdoc />
-        public bool IsAsymmetric { get; } = true;
+        public bool IsAsymmetric =>
+            true;
 
         /// <inheritdoc />
         public byte[] Sign(byte[] key, byte[] bytesToSign) =>
             Sign(bytesToSign);
 
-        public byte[] Sign(byte[] bytesToSign)
-        {
-            if (!_cert.HasPrivateKey)
-                throw new CryptographicException("Certificate doesn't contain private key");
-
-            var privateKey = GetPrivateKey(_cert);
-            return privateKey.SignData(bytesToSign, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-        }
+        /// <summary>
+        /// Signs the provided bytes.
+        /// </summary>
+        /// <param name="bytesToSign">The bytes to sign.</param>
+        /// <returns>The signed bytes.</returns>
+        public byte[] Sign(byte[] bytesToSign) =>
+            _privateKey.SignData(bytesToSign, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
 
         /// <summary>
         /// Verifies provided byte array with provided signature.
@@ -42,8 +58,9 @@ namespace JWT.Algorithms
         /// <param name="signature">The signature to verify with</param>
         public bool Verify(byte[] bytesToSign, byte[] signature)
         {
-            var publicKey = GetPublicKey(_cert);
-            return publicKey.VerifyData(bytesToSign, "2.16.840.1.101.3.4.2.1", signature);
+            // 2.16.840.1.101.3.4.2.1 is the object id for the sha256NoSign algorithm.
+            // See https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-gpnap/a48b02b2-2a10-4eb0-bed4-1807a6d2f5ad for further details.
+            return _publicKey.VerifyData(bytesToSign, "2.16.840.1.101.3.4.2.1", signature);
         }
 
         private static RSA GetPrivateKey(X509Certificate2 cert)
