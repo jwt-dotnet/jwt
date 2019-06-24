fe22ed909711dbbe4835d167efac5a4371b095c3
Alexander Batishchev <abatishchev@gmail.com>
2019-06-24 11:47:27 -0700

    Checking for public and private key not to be null in RS256Algorithm

--- src/JWT/Algorithms/RS256Algorithm.cs
+++ src/JWT/Algorithms/RS256Algorithm.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 namespace JWT.Algorithms
@@ -47,19 +48,19 @@ namespace JWT.Algorithms
         /// <param name="bytesToSign">The bytes to sign.</param>
         /// <returns>The signed bytes.</returns>
         public byte[] Sign(byte[] bytesToSign) =>
-            _privateKey.SignData(bytesToSign, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            (_privateKey ?? throw new InvalidOperationException("Private key is null")).SignData(bytesToSign, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
 
         /// <summary>
         /// Verifies provided byte array with provided signature.
         /// </summary>
+        /// <remarks>
+        /// 2.16.840.1.101.3.4.2.1 is the object id for the sha256NoSign algorithm.
+        /// See https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-gpnap/a48b02b2-2a10-4eb0-bed4-1807a6d2f5ad for further details.
+        /// </remarks>
         /// <param name="bytesToSign">The data to verify</param>
         /// <param name="signature">The signature to verify with</param>
-        public bool Verify(byte[] bytesToSign, byte[] signature)
-        {
-            // 2.16.840.1.101.3.4.2.1 is the object id for the sha256NoSign algorithm.
-            // See https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-gpnap/a48b02b2-2a10-4eb0-bed4-1807a6d2f5ad for further details.
-            return _publicKey.VerifyData(bytesToSign, "2.16.840.1.101.3.4.2.1", signature);
-        }
+        public bool Verify(byte[] bytesToSign, byte[] signature) =>
+            (_publicKey ?? throw new InvalidOperationException("Public key is null")).VerifyData(bytesToSign, "2.16.840.1.101.3.4.2.1", signature);
 
         private static RSA GetPrivateKey(X509Certificate2 cert)
         {
