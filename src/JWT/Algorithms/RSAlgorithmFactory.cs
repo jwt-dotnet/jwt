85ac972f416969bf2723c91f78eb69e0d996c43e
flerka <marya.kozhemyakina@gmail.com>
2019-09-26 22:16:07 +0300

    Extend RSAlgorithmFactory to support all ctors of RS256Algorithm

--- src/JWT/Algorithms/RSAlgorithmFactory.cs
+++ src/JWT/Algorithms/RSAlgorithmFactory.cs
@@ -8,13 +8,25 @@ namespace JWT.Algorithms
     public sealed class RSAlgorithmFactory : HMACSHAAlgorithmFactory
     {
         private readonly Func<X509Certificate2> _certFactory;
+        private readonly Func<RS256Algorithm> _createAlgorithmMethod;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RSAlgorithmFactory"/> class
         /// </summary>
         /// <param name="certFactory">Func that returns <see cref="X509Certificate2" /> which will be used to instantiate <see cref="RS256Algorithm" /></param>
-        public RSAlgorithmFactory(Func<X509Certificate2> certFactory) =>
+        public RSAlgorithmFactory(Func<X509Certificate2> certFactory)
+        {
             _certFactory = certFactory;
+            _createAlgorithmMethod = CreateWithCertificate;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RSAlgorithmFactory"/> class
+        /// </summary>
+        /// <param name="publicKey">The RSA service provider which will be used to instantiate <see cref="RS256Algorithm" /></param>
+        public RSAlgorithmFactory(RSACryptoServiceProvider publicKey) =>
+            _createAlgorithmMethod = () => new RS256Algorithm(publicKey);
+            
 
         /// <inheritdoc />
         public override IJwtAlgorithm Create(JwtHashAlgorithm algorithm)
@@ -22,17 +34,22 @@ namespace JWT.Algorithms
             switch (algorithm)
             {
                 case JwtHashAlgorithm.RS256:
-                {
-                    var certificate = _certFactory();
-#if NETSTANDARD1_3
-                    return new RS256Algorithm((RSACryptoServiceProvider)certificate.GetRSAPublicKey(), certificate.GetRSAPrivateKey());
-#else
-                    return new RS256Algorithm((RSACryptoServiceProvider)certificate.PublicKey.Key, (RSA)certificate.PrivateKey);
-#endif
-                }
+                    {
+                        return _createAlgorithmMethod();
+                    }
                 default:
                     throw new NotSupportedException($"For algorithm {Enum.GetName(typeof(JwtHashAlgorithm), algorithm)} please use the appropriate factory by implementing {nameof(IAlgorithmFactory)}");
             }
+        }
+
+        private RS256Algorithm CreateWithCertificate()
+        {
+            var certificate = _certFactory();
+#if NETSTANDARD1_3
+            return new RS256Algorithm((RSACryptoServiceProvider)certificate.GetRSAPublicKey(), certificate.GetRSAPrivateKey());
+#else
+            return new RS256Algorithm((RSACryptoServiceProvider)certificate.PublicKey.Key, (RSA)certificate.PrivateKey);
+#endif
         }
     }
 }
