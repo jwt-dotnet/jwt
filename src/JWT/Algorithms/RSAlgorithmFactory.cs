0af359313413ff0ace9fde11c77b71f1fc046b09
Alexander Batishchev <abatishchev@gmail.com>
2019-10-01 11:01:02 -0700

    Update RSAlgorithmFactory.cs

--- src/JWT/Algorithms/RSAlgorithmFactory.cs
+++ src/JWT/Algorithms/RSAlgorithmFactory.cs
@@ -14,14 +14,11 @@ namespace JWT.Algorithms
         /// </summary>
         /// <param name="certFactory">Func that returns <see cref="X509Certificate2" /> which will be used to instantiate <see cref="RS256Algorithm" /></param>
         public RSAlgorithmFactory(Func<X509Certificate2> certFactory) =>
-            _algFactory = () =>new RS256Algorithm(certFactory());
+            _algFactory = () => new RS256Algorithm(certFactory());
 
         /// <summary>
         /// Creates an instance of <see cref="RSAlgorithmFactory"/> using the provided public key only.
         /// </summary>
-        /// <remarks>
-        /// An instance of <see cref="RSAlgorithmFactory" /> created using this constructor can only be used for verifying the data, not for signing it.
-        /// </remarks>
         /// <param name="publicKey">The public key for verifying the data.</param>
         public RSAlgorithmFactory(RSA publicKey) =>
             _algFactory = () => new RS256Algorithm(publicKey);
