2de2bd07e1ee07e8cc148a35716787cfb96d5924
flerka <6013716+flerka@users.noreply.github.com>
2019-10-01 21:12:36 +0300

    Extend RSAlgorithmFactory to support all ctors of RS256Algorithm (#221)
    * Extend RSAlgorithmFactory to support all ctors of RS256Algorithm
    * Update JWT.csproj to bump version

--- src/JWT/Algorithms/RSAlgorithmFactory.cs
+++ src/JWT/Algorithms/RSAlgorithmFactory.cs
@@ -7,14 +7,29 @@ namespace JWT.Algorithms
     /// <inheritdoc />
     public sealed class RSAlgorithmFactory : HMACSHAAlgorithmFactory
     {
-        private readonly Func<X509Certificate2> _certFactory;
+        private readonly Func<RS256Algorithm> _algFactory;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="RSAlgorithmFactory"/> class
+        /// Creates an instance of the <see cref="RSAlgorithmFactory" /> class using the provided <see cref="X509Certificate2" />.
         /// </summary>
         /// <param name="certFactory">Func that returns <see cref="X509Certificate2" /> which will be used to instantiate <see cref="RS256Algorithm" /></param>
         public RSAlgorithmFactory(Func<X509Certificate2> certFactory) =>
-            _certFactory = certFactory;
+            _algFactory = () => new RS256Algorithm(certFactory());
+
+        /// <summary>
+        /// Creates an instance of <see cref="RSAlgorithmFactory"/> using the provided public key only.
+        /// </summary>
+        /// <param name="publicKey">The public key for verifying the data.</param>
+        public RSAlgorithmFactory(RSA publicKey) =>
+            _algFactory = () => new RS256Algorithm(publicKey);
+
+        /// <summary>
+        /// Creates an instance of <see cref="RSAlgorithmFactory"/> using the provided pair of public and private keys.
+        /// </summary>
+        /// <param name="publicKey">The public key for verifying the data.</param>
+        /// <param name="privateKey">The private key for signing the data.</param>
+        public RSAlgorithmFactory(RSA publicKey, RSA privateKey) =>
+            _algFactory = () => new RS256Algorithm(publicKey, privateKey);
 
         /// <inheritdoc />
         public override IJwtAlgorithm Create(JwtHashAlgorithm algorithm)
@@ -22,14 +37,7 @@ namespace JWT.Algorithms
             switch (algorithm)
             {
                 case JwtHashAlgorithm.RS256:
-                {
-                    var certificate = _certFactory();
-#if NETSTANDARD1_3
-                    return new RS256Algorithm((RSACryptoServiceProvider)certificate.GetRSAPublicKey(), certificate.GetRSAPrivateKey());
-#else
-                    return new RS256Algorithm((RSACryptoServiceProvider)certificate.PublicKey.Key, (RSA)certificate.PrivateKey);
-#endif
-                }
+                    return _algFactory();
                 default:
                     throw new NotSupportedException($"For algorithm {Enum.GetName(typeof(JwtHashAlgorithm), algorithm)} please use the appropriate factory by implementing {nameof(IAlgorithmFactory)}");
             }
