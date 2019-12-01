3300ed023c535e7a90c6d09cdd4efd8ebfaaf3d5
Alexander Batishchev <abatishchev@gmail.com>
2019-12-01 10:27:47 -0800

    lic

---
+++ src/JWT.Extensions.AspNetCore/AuthenticationAppBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Alexander Batishchev. All rights reserved.
+// Licensed under the MIT License. See License.md in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Authentication;
+
+namespace JWT
+{
+    public static class AuthenticationAppBuilderExtensions
+    {
+        public const string SchemaName = "Jwt";
+
+        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder)
+        {
+            return builder.AddJwt(SchemaName);
+        }
+
+        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder,
+            string authenticationScheme)
+        {
+            return builder.AddJwt(authenticationScheme, (Action<JwtAuthenticationOptions>)null);
+        }
+
+        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder, Action<JwtAuthenticationOptions> configureOptions)
+        {
+            return builder.AddJwt(SchemaName, configureOptions);
+        }
+
+        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder, string authenticationScheme, Action<JwtAuthenticationOptions> configureOptions)
+        {
+            return builder.AddScheme<JwtAuthenticationOptions, JwtAuthenticationHandler>(authenticationScheme, configureOptions);
+        }
+    }
+}
