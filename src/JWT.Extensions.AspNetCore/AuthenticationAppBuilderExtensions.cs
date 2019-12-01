0922b6aeeee27b3eba778e546e6427cf51d48605
Alexander Batishchev <abatishchev@gmail.com>
2019-12-01 12:39:54 -0800

    ext

--- src/JWT.Extensions.AspNetCore/AuthenticationAppBuilderExtensions.cs
+++ src/JWT.Extensions.AspNetCore/AuthenticationAppBuilderExtensions.cs
@@ -10,25 +10,16 @@ namespace JWT
     {
         public const string SchemaName = "Jwt";
 
-        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder)
-        {
-            return builder.AddJwt(SchemaName);
-        }
+        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder) =>
+            builder.AddJwt(SchemaName);
 
-        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder,
-            string authenticationScheme)
-        {
-            return builder.AddJwt(authenticationScheme, (Action<JwtAuthenticationOptions>)null);
-        }
+        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder, string authenticationScheme) =>
+            builder.AddJwt(authenticationScheme, null);
 
-        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder, Action<JwtAuthenticationOptions> configureOptions)
-        {
-            return builder.AddJwt(SchemaName, configureOptions);
-        }
+        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder, Action<JwtAuthenticationOptions> configureOptions) =>
+            builder.AddJwt(SchemaName, configureOptions);
 
-        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder, string authenticationScheme, Action<JwtAuthenticationOptions> configureOptions)
-        {
-            return builder.AddScheme<JwtAuthenticationOptions, JwtAuthenticationHandler>(authenticationScheme, configureOptions);
-        }
+        public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder, string authenticationScheme, Action<JwtAuthenticationOptions> configureOptions) =>
+            builder.AddScheme<JwtAuthenticationOptions, JwtAuthenticationHandler>(authenticationScheme, configureOptions);
     }
 }
--- src/JWT.Extensions.AspNetCore/ServiceCollectionExtensions.cs
+++ src/JWT.Extensions.AspNetCore/ServiceCollectionExtensions.cs
@@ -15,6 +15,21 @@ namespace JWT
         /// Adds authentication/authorization using JWT to the specified <see cref="IServiceCollection" />.
         /// </summary>
         /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+        /// <returns>
+        /// The <see cref="IServiceCollection" />.
+        /// </returns>
+        public static IServiceCollection AddJwt(this IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds authentication/authorization using JWT to the specified <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
         /// <param name="configureOptions">An <see cref="Action{JwtOptions}"/> to configure the provided <see cref="JwtOptions"/>.</param>
         /// <returns>
         /// The <see cref="IServiceCollection" />.
@@ -23,7 +38,6 @@ namespace JWT
         {
             if (services == null)
                 throw new ArgumentNullException(nameof(services));
-
             if (configureOptions == null)
                 throw new ArgumentNullException(nameof(configureOptions));
 
