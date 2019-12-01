b286dfb414e289a910223fca8882ad8f769e8242
Alexander Batishchev <abatishchev@gmail.com>
2019-12-01 10:32:18 -0800

    handler

---
+++ src/JWT.Extensions.AspNetCore/JwtAuthenticationHandler.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Alexander Batishchev. All rights reserved.
+// Licensed under the MIT License. See License.md in the project root for license information.
+
+using System;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace JWT
+{
+    public sealed class JwtAuthenticationHandler : AuthenticationHandler<JwtAuthenticationOptions>
+    {
+        public JwtAuthenticationHandler(IOptionsMonitor<JwtAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/JWT.Extensions.AspNetCore/JwtAuthenticationOptions.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Alexander Batishchev. All rights reserved.
+// Licensed under the MIT License. See License.md in the project root for license information.
+
+using Microsoft.AspNetCore.Authentication;
+
+namespace JWT
+{
+    public class JwtAuthenticationOptions : AuthenticationSchemeOptions
+    {
+    }
+}
