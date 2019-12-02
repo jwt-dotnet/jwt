01ed7e8fc4b715fe447292aec8e489a0b0fb48fc
Alexander Batishchev <abatishchev@gmail.com>
2019-12-01 22:48:08 -0800

    tests

--- tests/JWT.Extensions.AspNetCore.Tests/JwtAuthenticationHandlerTests.cs
+++ tests/JWT.Extensions.AspNetCore.Tests/JwtAuthenticationHandlerTests.cs
@@ -1,24 +1,21 @@
 ﻿using System.Text.Encodings.Web;
 using System.Threading.Tasks;
-using AutoFixture;
 using FluentAssertions;
 using JWT.Serializers;
 using JWT.Tests.Common.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace JWT.Extensions.AspNetCore.Tests
 {
     [TestClass]
     public class JwtAuthenticationHandlerTests
     {
-        private readonly Fixture _fixture = new Fixture();
-
         [TestMethod]
         public async Task HandleAuthenticateAsync_Should_Return_AuthenticateResult_Success()
         {
@@ -29,32 +26,30 @@ namespace JWT.Extensions.AspNetCore.Tests
             var jwtValidator = new JwtValidator(serializer, dateTimeProvider);
             var decoder = new JwtDecoder(serializer, jwtValidator, urlEncoder);
 
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddOptions<JwtAuthenticationOptions>()
-                             .Configure(options =>
-                                  {
-                                      options.Keys = new[] { _fixture.Create<string>() };
-                                      options.VerifySignature = true;
-                                  });
-            var optionsMonitor = serviceCollection.BuildServiceProvider()
-                                                  .GetService<IOptionsMonitor<JwtAuthenticationOptions>>();
+            var options = new JwtAuthenticationOptions
+            {
+                Keys = new[] { TestData.Key },
+                VerifySignature = true
+            };
+            var optionsMonitor = new Mock<IOptionsMonitor<JwtAuthenticationOptions>>();
+            optionsMonitor.Setup(m => m.Get(It.IsAny<string>())).Returns(options);
 
             var loggerFactory = new NullLoggerFactory();
             var encoder = UrlEncoder.Default;
             var clock = new SystemClock();
 
-            var handler = new JwtAuthenticationHandler(decoder, optionsMonitor, loggerFactory, encoder, clock);
+            var handler = new JwtAuthenticationHandler(decoder, optionsMonitor.Object, loggerFactory, encoder, clock);
 
             var scheme = new AuthenticationScheme(JwtAuthenticationDefaults.AuthenticationScheme, JwtAuthenticationDefaults.AuthenticationScheme, typeof(JwtAuthenticationHandler));
             var context = new DefaultHttpContext
             {
                 Request =
+            {
+                Headers =
                 {
-                    Headers =
-                    {
-                        { HeaderNames.Authorization, $"{JwtAuthenticationDefaults.AuthenticationScheme} {TestData.Token}" }
-                    }
+                    { HeaderNames.Authorization, $"{JwtAuthenticationDefaults.AuthenticationScheme} {TestData.Token}" }
                 }
+            }
             };
 
             await handler.InitializeAsync(scheme, context);
