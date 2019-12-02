feec064a36f21e09da23d23e6dbcfa87e1c1a959
Alexander Batishchev <abatishchev@gmail.com>
2019-12-01 21:53:13 -0800

    owin ext

---
+++ src/JWT.Extensions.Owin/AppBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Owin;
+
+namespace JWT
+{
+    public static class AppBuilderExtensions
+    {
+        public static void UseClientCertificateAuthentication(this IAppBuilder appBuilder) =>
+            appBuilder.Use<JwtAuthenticationMiddleware>();
+    }
+}
