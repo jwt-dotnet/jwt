0350f2325fb2f203431391fbee3007c43105a7c3
Alexander Batishchev <abatishchev@gmail.com>
2020-01-05 16:47:17 -0500

    Update DelegateAlgorithmFactory.cs

--- src/JWT/Algorithms/DelegateAlgorithmFactory.cs
+++ src/JWT/Algorithms/DelegateAlgorithmFactory.cs
@@ -1,16 +1,22 @@
-
-
 namespace JWT.Algorithms
 {
-    /// <inheritdoc />
-    public sealed class RSAlgorithmFactory : HMACSHAAlgorithmFactory
+    /// <summary>
+    /// Implements <see href="IAlgorithmFactory" /> by returning the supplied algorithm.
+    /// <summary>
+    public sealed class DelegateAlgorithmFactory : IAlgorithmFactory
     {
-        private readonly Func<RS256Algorithm> _algFactory;
+        private readonly Func<IAlgorithm> _algFactory
 
+        public DelegateAlgorithmFactory(Func<IAlgorithm> algFactory) =>
+            _algFactory = algFactory;
+
+        public DelegateAlgorithmFactory(IAlgorithm algorithm)
+            : this(() => algorithm)
+        {
+        }
 
         /// <inheritdoc />
-        public override IJwtAlgorithm Create(JwtHashAlgorithm algorithm)
-        {
-       
+        public IJwtAlgorithm Create(IAlgorithm algorithm)
+            _algFactory();
     }
 }
