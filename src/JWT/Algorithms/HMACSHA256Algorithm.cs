2c9daf2972fa349bd458d2a01435060b5096e639
Alexander Batishchev <abatishchev@gmail.com>
2020-04-04 10:06:22 -0700

    sha

--- src/JWT/Algorithms/HMACSHA256Algorithm.cs
+++ src/JWT/Algorithms/HMACSHA256Algorithm.cs
@@ -10,16 +10,11 @@ namespace JWT.Algorithms
         /// <inheritdoc />
         public byte[] Sign(byte[] key, byte[] bytesToSign)
         {
-            using (var sha = new HMACSHA256(key))
-            {
-                return sha.ComputeHash(bytesToSign);
-            }
+            using var sha = new HMACSHA256(key);
+            return sha.ComputeHash(bytesToSign);
         }
 
         /// <inheritdoc />
         public string Name => JwtHashAlgorithm.HS256.ToString();
-
-        /// <inheritdoc />
-        public bool IsAsymmetric => false;
     }
 }
--- src/JWT/Algorithms/HMACSHA384Algorithm.cs
+++ src/JWT/Algorithms/HMACSHA384Algorithm.cs
@@ -10,16 +10,11 @@ namespace JWT.Algorithms
         /// <inheritdoc />
         public byte[] Sign(byte[] key, byte[] bytesToSign)
         {
-            using (var sha = new HMACSHA384(key))
-            {
-                return sha.ComputeHash(bytesToSign);
-            }
+            using var sha = new HMACSHA384(key);
+            return sha.ComputeHash(bytesToSign);
         }
 
         /// <inheritdoc />
         public string Name => JwtHashAlgorithm.HS384.ToString();
-
-        /// <inheritdoc />
-        public bool IsAsymmetric => false;
     }
 }
