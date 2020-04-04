2570420d9078d20fbbeea57a1927c5f59ce89db1
Alexander Batishchev <abatishchev@gmail.com>
2020-04-04 10:14:12 -0700

    ext

---
+++ src/JWT/Algorithms/JwtAlgorithmExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace JWT.Algorithms
+{
+    /// <summary>
+    /// Extension methods for <seealso cref="IJwtAlgorithm" />
+    ///</summary>
+    public static class JwtAlgorithmExtensions
+    {
+        public static bool IsAsymmetric(this IJwtAlgorithm alg) =>
+            alg is IAsymmetricAlgorithm;
+    }
+}
