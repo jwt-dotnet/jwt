d8a462ef1063fa21a7462dceee6073767ea2eda5
Gerke Geurts <ggeurts@agg-solutions.com>
2020-04-17 11:10:50 +0200

    Solved .NET Framework test failures

--- src/JWT/Compatibility/System.Security.Cryptography/HashAlgorithName.cs
+++ src/JWT/Compatibility/System.Security.Cryptography/HashAlgorithName.cs
@@ -1,50 +1,13 @@
 ﻿#if NET35 || NET40
 namespace System.Security.Cryptography
 {
-    internal struct HashAlgorithmName : IEquatable<HashAlgorithmName>
+    internal static class HashAlgorithmName
     {
-        public static HashAlgorithmName MD5 => new HashAlgorithmName("MD5");
-        public static HashAlgorithmName SHA1 => new HashAlgorithmName("SHA1");
-        public static HashAlgorithmName SHA256 => new HashAlgorithmName("SHA256");
-        public static HashAlgorithmName SHA384 => new HashAlgorithmName("SHA384");
-        public static HashAlgorithmName SHA512 => new HashAlgorithmName("SHA512");
-
-        public string Name { get; }
-
-        public HashAlgorithmName(string name)
-        {
-            this.Name = name;
-        }
-
-        public static bool operator ==(HashAlgorithmName left, HashAlgorithmName right)
-        {
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(HashAlgorithmName left, HashAlgorithmName right)
-        {
-            return !(left == right);
-        }
-
-        public bool Equals(HashAlgorithmName other)
-        {
-            return this.Name == other.Name;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is HashAlgorithmName other && Equals(other);
-        }
-
-        public override int GetHashCode()
-        {
-            return this.Name?.GetHashCode() ?? 0;
-        }
-
-        public override string ToString()
-        {
-            return this.Name ?? string.Empty;
-        }
+        public static readonly string MD5 = nameof(MD5);
+        public static readonly string SHA1 = nameof(SHA1);
+        public static readonly string SHA256 = nameof(SHA256);
+        public static readonly string SHA384 = nameof(SHA384);
+        public static readonly string SHA512 = nameof(SHA512);
     }
 }
 #endif
