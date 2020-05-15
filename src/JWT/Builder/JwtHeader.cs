a63c9109d6b4e40d64999e9284d9cb5b71b88358
Luca Poretti <luca@qualitando.com>
2020-05-15 23:00:55 +0200

    fix #218

--- src/JWT/Builder/JwtHeader.cs
+++ src/JWT/Builder/JwtHeader.cs
@@ -1,4 +1,7 @@
 ﻿using Newtonsoft.Json;
+#if SYSTEMTEXTJSON
+using System.Text.Json.Serialization;
+#endif
 
 namespace JWT.Builder
 {
@@ -8,24 +11,45 @@ namespace JWT.Builder
     public class JwtHeader
     {
         [JsonProperty("typ")]
+#if SYSTEMTEXTJSON
+        [JsonPropertyName("typ")]
+#endif
         public string Type { get; set; }
 
         [JsonProperty("cty")]
+#if SYSTEMTEXTJSON
+        [JsonPropertyName("cty")]
+#endif
         public string ContentType { get; set; }
 
         [JsonProperty("alg")]
+#if SYSTEMTEXTJSON
+        [JsonPropertyName("alg")]
+#endif
         public string Algorithm { get; set; }
 
         [JsonProperty("kid")]
+#if SYSTEMTEXTJSON
+        [JsonPropertyName("kid")]
+#endif
         public string KeyId { get; set; }
 
         [JsonProperty("x5u")]
+#if SYSTEMTEXTJSON
+        [JsonPropertyName("x5u")]
+#endif
         public string X5u { get; set; }
 
         [JsonProperty("x5c")]
+#if SYSTEMTEXTJSON
+        [JsonPropertyName("x5c")]
+#endif
         public string X5c { get; set; }
 
         [JsonProperty("x5t")]
+#if SYSTEMTEXTJSON
+        [JsonPropertyName("x5t")]
+#endif
         public string X5t { get; set; }
     }
 }
---
+++ src/JWT/Serializers/SystemTextJsonSerializer.cs
@@ -0,0 +1,60 @@
+﻿#if SYSTEMTEXTJSON
+namespace JWT.Serializers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.Json;
+    using JWT;
+
+    public class SystemTextJsonSerializer : IJsonSerializer
+    {
+        public string Serialize(object obj)
+        {
+            // Implement using favorite JSON serializer
+            return JsonSerializer.Serialize(obj);
+        }
+
+        public T Deserialize<T>(string json)
+        {
+            // Implement using favorite JSON serializer
+            var data = JsonSerializer.Deserialize<T>(json);
+
+            if (data is Dictionary<string, object> odata)
+            {
+                var ndata = new Dictionary<string, object>();
+                foreach (var key in odata.Keys)
+                {
+                    var value = (JsonElement)odata[key];
+                    switch (value.ValueKind)
+                    {
+                        case JsonValueKind.Undefined:
+                            break;
+                        case JsonValueKind.Object:
+                            break;
+                        case JsonValueKind.Array:
+                            break;
+                        case JsonValueKind.String:
+                            ndata.Add(key, value.GetString());
+                            break;
+                        case JsonValueKind.Number:
+                            ndata.Add(key, value.GetInt64());
+                            break;
+                        case JsonValueKind.True:
+                            ndata.Add(key, true);
+                            break;
+                        case JsonValueKind.False:
+                            ndata.Add(key, false);
+                            break;
+                        case JsonValueKind.Null:
+                            break;
+                    }
+                }
+
+                if (ndata is T obj) return obj;
+            }
+
+            return data;
+        }
+    }
+}
+#endif
