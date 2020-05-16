b2a139820aacabf15f28972950ec779cdb3b4430
Luca Poretti <luca@qualitando.com>
2020-05-16 10:00:19 +0200

    code cleanup

--- src/JWT/Serializers/SystemTextJsonSerializer.cs
+++ src/JWT/Serializers/SystemTextJsonSerializer.cs
@@ -5,58 +5,48 @@ using System.Collections.Generic;
 using System.Text.Json;
 using JWT;
 
-
 namespace JWT.Serializers
 {
     public sealed class SystemTextJsonSerializer : IJsonSerializer
     {
         public string Serialize(object obj)
         {
-            // Implement using favorite JSON serializer
             return JsonSerializer.Serialize(obj);
         }
 
         public T Deserialize<T>(string json)
         {
-            // Implement using favorite JSON serializer
             var data = JsonSerializer.Deserialize<T>(json);
 
-            if (data is Dictionary<string, object> odata)
-            {
-                var ndata = new Dictionary<string, object>();
-                foreach (var key in odata.Keys)
-                {
-                    var value = (JsonElement)odata[key];
-                    switch (value.ValueKind)
-                    {
-                        case JsonValueKind.Undefined:
-                            break;
-                        case JsonValueKind.Object:
-                            break;
-                        case JsonValueKind.Array:
-                            break;
-                        case JsonValueKind.String:
-                            ndata.Add(key, value.GetString());
-                            break;
-                        case JsonValueKind.Number:
-                            ndata.Add(key, value.GetInt64());
-                            break;
-                        case JsonValueKind.True:
-                            ndata.Add(key, true);
-                            break;
-                        case JsonValueKind.False:
-                            ndata.Add(key, false);
-                            break;
-                        case JsonValueKind.Null:
-                            break;
-                    }
-                }
+            if (!(data is Dictionary<string, JsonElement> odata)) return data;
 
-                if (ndata is T obj)
-                    return obj;
+            var ndata = new Dictionary<string, object>();
+            foreach (var key in odata.Keys)
+            {
+                var value = odata[key];
+                switch (value.ValueKind)
+                {
+                    case JsonValueKind.String:
+                        ndata.Add(key, value.GetString());
+                        break;
+                    case JsonValueKind.Number:
+                        ndata.Add(key, value.GetInt64());
+                        break;
+                    case JsonValueKind.True:
+                        ndata.Add(key, true);
+                        break;
+                    case JsonValueKind.False:
+                        ndata.Add(key, false);
+                        break;
+                    case JsonValueKind.Undefined:
+                    case JsonValueKind.Object:
+                    case JsonValueKind.Array:
+                    case JsonValueKind.Null:
+                        throw new NotSupportedException(nameof(value.ValueKind));
+                }
             }
 
-            return data;
+            return ndata is T obj ? obj : data;
         }
     }
 }
