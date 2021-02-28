5e12c8b75c3a62dde59baee7d0baadd81f9927cc
Alexander Batishchev <abatishchev@gmail.com>
2021-02-28 15:55:03 -0800

    readonly

--- src/JWT/Compatibility/System.Security.Cryptography/HashAlgorithmName.cs
+++ src/JWT/Compatibility/System.Security.Cryptography/HashAlgorithmName.cs
@@ -1,7 +1,7 @@
 #if NET35 || NET40
 namespace System.Security.Cryptography
 {
-    public struct HashAlgorithmName
+    public readonly struct HashAlgorithmName
     {
         public HashAlgorithmName(string name) =>
             this.Name = name;
