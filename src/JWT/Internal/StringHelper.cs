8262145e670a08ecc1589b1948587cdd90a8ad6c
Alexander Batishchev <abatishchev@gmail.com>
2021-04-09 17:39:37 -0700

    Update StringHelper.cs

--- src/JWT/Internal/StringHelper.cs
+++ src/JWT/Internal/StringHelper.cs
@@ -1,20 +1,18 @@
-﻿namespace JWT.Internal
+using System;
+
+namespace JWT.Internal
 {
     internal static class StringHelper
     {
         internal static string FirstSegment(this string input, char separator)
         {
-            int idx = input.IndexOf(separator);
-            if (idx != -1)
-            {
-                return input.Substring(0, idx);
-            }
-            return input;
+            var idx = input.IndexOf(separator, StringComparison.OrdinalIgnoreCase);
+            return idx != -1 ? input.Substring(0, idx): input;
         }
 
         internal static int Count(this string input, char character)
         {
-            int counter = 0;
+            var counter = 0;
             for (int i = 0; i < input.Length; i++)
             {
                 if (input[i] == character)
