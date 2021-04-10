20bf60c03e9d64485b51c1b3971d04b6815b2bf3
Alexander Batishchev <abatishchev@gmail.com>
2021-04-09 23:37:11 -0700

    Update String.cs

--- src/JWT/Compatibility/String.cs
+++ src/JWT/Compatibility/String.cs
@@ -1,32 +1,20 @@
-using System;
-
+#if NET35
 namespace JWT.Compatibility
 {
     internal static class String
     {
-#if !NETSTANDARD2_0
-        /// <remarks>
-        /// The overload accpting char is missing outside .NET Standard.
-        /// See https://docs.microsoft.com/en-us/dotnet/api/system.string.indexof?view=netstandard-2.0#System_String_IndexOf_System_String_System_StringComparison_
-        /// </remarks>
-        public static int IndexOf(this string value, char ch, StringComparison comparisonType)
-        {
-            return value.IndexOf(ch.ToString(), comparisonType);
-        }
-#endif
-  
-#if NET35
         public static bool IsNullOrWhiteSpace(string value)
         {
             if (!string.IsNullOrEmpty(value))
             {
                 foreach (var ch in value)
                 {
-                    if (!char.IsWhiteSpace(ch)) return false;
+                    if (!char.IsWhiteSpace(ch))
+                        return false;
                 }
             }
             return true;
         }
-#endif
     }
 }
+#endif
