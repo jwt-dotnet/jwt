7ac53f3123e632dc93cd3aece0151a8d9f13c3b3
Alexander Batishchev <abatishchev@gmail.com>
2021-09-28 15:01:36 -0700

    cs

--- src/JWT/Internal/EncodingHelper.cs
+++ src/JWT/Internal/EncodingHelper.cs
@@ -7,7 +7,7 @@ namespace JWT.Internal
         private static readonly UTF8Encoding utf8Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
 
         public static byte[] GetBytes(string input) =>
-            input is object ? utf8Encoding.GetBytes(input) : null;
+            utf8Encoding.GetBytes(input);
 
         public static byte[] GetBytes(string input1, byte separator, string input2)
         {
