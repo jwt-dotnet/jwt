31959a6a1fef11f7cb5d0d7d09ffd884e367ac94
Alexander Batishchev <abatishchev@gmail.com>
2022-01-24 12:30:24 -0800

    tests

---
+++ tests/JWT.Tests.Net40/TargetFrameworkTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JWT.Tests
+{
+    [TestClass]
+    public class TargetFrameworkTests
+    {
+        [TestMethod]
+#if NET40
+        [ExpectedException(typeof(System.InvalidOperationException))]
+#endif
+        public void Build_Must_Fail_When_TargetFramework_Is_Incorrect()
+        {
+#if NET40
+            throw new System.InvalidOperationException();
+#endif
+        }
+    }
+}
