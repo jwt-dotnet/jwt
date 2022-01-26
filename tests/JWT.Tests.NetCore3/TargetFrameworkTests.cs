20378ec58608119b08f7d199fb4064f2d1b0fab1
Alexander Batishchev <abatishchev@gmail.com>
2022-01-26 10:42:07 -0800

    Refactored referencing common tests project (#371)
    * Added Directory.Build.props for tests only
    * Adding test, common files automatically but conditionally
    * Added tests for target framework

---
+++ tests/JWT.Tests.NetCore3/TargetFrameworkTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JWT.Tests
+{
+    [TestClass]
+    public class TargetFrameworkTests
+    {
+        [TestMethod]
+#if NETSTANDARD2_0 || NETCOREAPP3_1
+        [ExpectedException(typeof(System.InvalidOperationException))]
+#endif
+        public void Build_Must_Fail_When_TargetFramework_Is_Incorrect()
+        {
+#if NETSTANDARD2_0 || NETCOREAPP3_1
+            throw new System.InvalidOperationException();
+#endif
+        }
+    }
+}
