0363ffb02bf1a9fe53497f5d88f1cb9e63bcd7ec
Alexander Batishchev <abatishchev@gmail.com>
2022-02-12 11:44:19 -0800

    Update JsonNetSerializer.cs

--- src/JWT/Serializers/JsonNetSerializer.cs
+++ src/JWT/Serializers/JsonNetSerializer.cs
@@ -16,6 +16,7 @@ namespace JWT.Serializers
         /// Creates a new instance of <see cref="JsonNetSerializer" />
         /// </summary>
         /// <remarks>Uses <see cref="JsonSerializer.CreateDefault()" /> as internal serializer</remarks>
+        /// <exception cref="ArgumentNullException" />
         public JsonNetSerializer()
             : this(JsonSerializer.CreateDefault())
         {
@@ -25,12 +26,17 @@ namespace JWT.Serializers
         /// Creates a new instance of <see cref="JsonNetSerializer" />
         /// </summary>
         /// <param name="serializer">Internal <see cref="JsonSerializer" /> to use for serialization</param>
+        /// <exception cref="ArgumentNullException" />
         public JsonNetSerializer(JsonSerializer serializer) =>
             _serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
 
         /// <inheritdoc />
+        /// <exception cref="ArgumentNullException" />
         public string Serialize(object obj)
         {
+            if (obj is null)
+                throw new ArgumentNullException(nameof(obj));
+
             var sb = new StringBuilder();
             using var stringWriter = new StringWriter(sb);
             using var jsonWriter = new JsonTextWriter(stringWriter);
@@ -39,11 +45,18 @@ namespace JWT.Serializers
         }
 
         /// <inheritdoc />
-        public T Deserialize<T>(string json)
+        /// <exception cref="ArgumentNullException" />
+        /// <exception cref="ArgumentException" />
+        public object Deserialize(Type type, string json)
         {
+            if (type is null)
+                throw new ArgumentNullException(nameof(type));
+            if (String.IsNullOrEmpty(json))
+                throw new ArgumentException(nameof(json));
+        
             using var stringReader = new StringReader(json);
             using var jsonReader = new JsonTextReader(stringReader);
-            return _serializer.Deserialize<T>(jsonReader);
+            return (T)_serializer.Deserialize(jsonReader, typeof(T));
         }
     }
 }
