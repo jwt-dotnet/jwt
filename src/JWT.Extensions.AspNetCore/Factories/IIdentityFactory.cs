8c06575df95b993bc2e0045f5ecfb203d3e5662c
Alexander Batishchev <abatishchev@gmail.com>
2022-02-12 11:49:20 -0800

    Update IIdentityFactory.cs

--- src/JWT.Extensions.AspNetCore/Factories/IIdentityFactory.cs
+++ src/JWT.Extensions.AspNetCore/Factories/IIdentityFactory.cs
@@ -1,10 +1,22 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Security.Principal;
 
 namespace JWT.Extensions.AspNetCore.Factories
 {
     public interface IIdentityFactory
     {
-        IIdentity CreateIdentity(IDictionary<string, string> payload);
+        /// <summary>
+        /// Creates <see cref="IIdentity" /> from the payload of the specified type.
+        /// </summary>
+        IIdentity CreateIdentity(Type type, object payload);
+    }
+    
+    /// <summary>
+    /// Extension methods for <seealso cref="IIdentityFactory" />
+    ///</summary>
+    public static class IdentityFactoryExtensions
+    {
+        public static IIdentity CreateIdentity<T>(this IIdentityFactory factory, T payload) =>
+            factory.CreateIdentity(typeof(T), payload);
     }
 }
