fd4deaf95d4ba752a9f9c6e5c1b5282f4a929a9b
Alexander Batishchev <abatishchev@gmail.com>
2022-06-03 10:22:16 -0700

    Update NoneAlgorithm.cs

--- src/JWT/Algorithms/NoneAlgorithm.cs
+++ src/JWT/Algorithms/NoneAlgorithm.cs
@@ -9,6 +9,10 @@ namespace JWT.Algorithms
     /// <see href="https://datatracker.ietf.org/doc/html/rfc7519#section-6">RFC-7519</see>
     public sealed class NoneAlgorithm : IJwtAlgorithm
     {
+        private static readonly Lazy<NoneAlgorithm> = new Lazy<NoneAlgorithm>(() => new NoneAlgorithm());
+
+        public static NoneAlgorithm Instance => _lazy.Value;
+
         /// <inheritdoc />
         public string Name => "none";
 
