95b5631ba585a0e1626fe4bd8cd24130d48bedab
Alexander Batishchev <abatishchev@gmail.com>
2022-07-27 19:44:00 -0700

    Renamed default IdentityFactory, opened up for inheritance, extension  (#428)
    * Renamed DefaultIdentityFactory.cs to ClaimsIdentityFactory.cs
    * Unsealed the class, made the methods virtual 
    * Bumped version to 10.0.0-beta3

--- src/JWT.Extensions.AspNetCore/Factories/ClaimsIdentityFactory.cs
+++ src/JWT.Extensions.AspNetCore/Factories/ClaimsIdentityFactory.cs
@@ -7,38 +7,37 @@ using Microsoft.Extensions.Options;
 
 namespace JWT.Extensions.AspNetCore.Factories
 {
-    public sealed class DefaultIdentityFactory : IIdentityFactory
+    public class ClaimsIdentityFactory : IIdentityFactory
     {
         private readonly IOptionsMonitor<JwtAuthenticationOptions> _options;
 
-        public DefaultIdentityFactory(IOptionsMonitor<JwtAuthenticationOptions> options) =>
+        public ClaimsIdentityFactory(IOptionsMonitor<JwtAuthenticationOptions> options) =>
             _options = options ?? throw new ArgumentNullException(nameof(options));
 
-        IIdentity IIdentityFactory.CreateIdentity(Type type, object payload)
+        public IIdentity CreateIdentity(Type type, object payload)
         {
             if (type is null)
                 throw new ArgumentNullException(nameof(type));
             if (payload is null)
                 throw new ArgumentException(nameof(payload));
 
+            var claims = ReadClaims(type, payload);
+            return CreateIdentity(claims);
+        }
+
+        protected virtual IEnumerable<Claim> ReadClaims(Type type, object payload)
+        {
             Type targetType = typeof(IDictionary<string, object>);
             if (!targetType.IsAssignableFrom(type))
                 throw new ArgumentOutOfRangeException(nameof(type), $"Type {type} is not assignable to {targetType}");
-
-            return CreateIdentity((IDictionary<string, object>)payload);
+ 
+            var dic = (IDictionary<string, object>)payload;
+            return dic.Select(p => new Claim(p.Key, p.Value.ToString()));
         }
 
-        /// <summary>
-        /// Creates user's identity from user's claims
-        /// </summary>
-        /// <param name="payload"><see cref="IDictionary{String,String}" /> of user's claims</param>
-        /// <returns><see cref="ClaimsIdentity" /></returns>
-        public IIdentity CreateIdentity(IDictionary<string, object> payload)
-        {
-            var claims = payload.Select(p => new Claim(p.Key, p.Value.ToString()));
-            return _options.CurrentValue.IncludeAuthenticationScheme ?
+        private IIdentity CreateIdentity(IEnumerable<Claim> claims) =>
+            _options.CurrentValue.IncludeAuthenticationScheme ?
                 new ClaimsIdentity(claims, JwtAuthenticationDefaults.AuthenticationScheme) :
                 new ClaimsIdentity(claims);
-        }
     }
 }
