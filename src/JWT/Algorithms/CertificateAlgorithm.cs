b38709443e6b96e5757f34b0f91beab869e03bcb
Alexander Batishchev <abatishchev@gmail.com>
2022-08-01 13:54:12 -0700

    Added base class CertificateAlgorithm (#432)
    * Added base class CertificateAlgorithm
    * Bumped version to 10.0.0-beta6

---
+++ src/JWT/Algorithms/CertificateAlgorithm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace JWT.Algorithms
+{
+    public abstract class CertificateAlgorithm<T> : IAsymmetricAlgorithm
+        where T : class
+    {
+        protected readonly T _publicKey;
+        protected readonly T _privateKey;
+
+        protected CertificateAlgorithm(T publicKey, T privateKey)
+        {
+            _publicKey = publicKey ?? throw new ArgumentNullException(nameof(publicKey));
+            _privateKey = privateKey ?? throw new ArgumentNullException(nameof(privateKey));
+        }
+
+        protected CertificateAlgorithm(T publicKey)
+        {
+            _publicKey = publicKey ?? throw new ArgumentNullException(nameof(publicKey));
+            _privateKey = null;
+        }
+
+        protected CertificateAlgorithm(X509Certificate2 cert)
+        {
+            if (cert is null)
+                throw new InvalidOperationException(nameof(cert));
+
+            _publicKey = GetPublicKey(cert) ?? throw new Exception("Certificate must have public key.");
+            _privateKey = GetPrivateKey(cert);
+        }
+
+        /// <inheritdoc />
+        public abstract string Name { get; }
+
+        /// <inheritdoc />
+        public abstract HashAlgorithmName HashAlgorithmName { get; }
+
+        /// <inheritdoc />
+        public byte[] Sign(byte[] key, byte[] bytesToSign) =>
+            Sign(bytesToSign);
+
+        /// <summary>
+        /// Signs the provided byte array with the private key.
+        /// </summary>
+        public byte[] Sign(byte[] bytesToSign)
+        {
+            if (bytesToSign is null)
+                throw new ArgumentNullException(nameof(bytesToSign));
+            if (_privateKey is null)
+                throw new InvalidOperationException("Can't sign data without private key");
+
+            return SignData(bytesToSign);
+        }
+
+        /// <inheritdoc />
+        public bool Verify(byte[] bytesToSign, byte[] signature)
+        {
+            if (bytesToSign is null)
+                throw new ArgumentNullException(nameof(bytesToSign));
+            if (signature is null)
+                throw new ArgumentNullException(nameof(signature));
+
+            return VerifyData(bytesToSign, signature);
+        }
+
+        protected abstract T GetPublicKey(X509Certificate2 cert);
+
+        protected abstract T GetPrivateKey(X509Certificate2 cert);
+
+        protected abstract byte[] SignData(byte[] bytesToSign);
+
+        protected abstract bool VerifyData(byte[] bytesToSign, byte[] signature);
+    }
+}
--- src/JWT/Algorithms/RSAlgorithm.cs
+++ src/JWT/Algorithms/RSAlgorithm.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 namespace JWT.Algorithms
@@ -7,20 +6,16 @@ namespace JWT.Algorithms
     /// <summary>
     /// RSASSA-PKCS1-v1_5 using SHA-256
     /// </summary>
-    public abstract class RSAlgorithm : IAsymmetricAlgorithm
+    public abstract class RSAlgorithm : CertificateAlgorithm<RSA>
     {
-        private readonly RSA _publicKey;
-        private readonly RSA _privateKey;
-
         /// <summary>
         /// Creates an instance of <see cref="RSAlgorithm" /> using the provided pair of public and private keys.
         /// </summary>
         /// <param name="publicKey">The public key for verifying the data.</param>
         /// <param name="privateKey">The private key for signing the data.</param>
         protected RSAlgorithm(RSA publicKey, RSA privateKey)
+            : base(publicKey, privateKey)
         {
-            _publicKey = publicKey ?? throw new ArgumentNullException(nameof(publicKey));
-            _privateKey = privateKey ?? throw new ArgumentNullException(nameof(privateKey));
         }
 
         /// <summary>
@@ -31,9 +26,8 @@ namespace JWT.Algorithms
         /// </remarks>
         /// <param name="publicKey">The public key for verifying the data.</param>
         protected RSAlgorithm(RSA publicKey)
+            : base(publicKey)
         {
-            _publicKey = publicKey ?? throw new ArgumentNullException(nameof(publicKey));
-            _privateKey = null;
         }
 
         /// <summary>
@@ -41,68 +35,36 @@ namespace JWT.Algorithms
         /// </summary>
         /// <param name="cert">The certificate having a public key and an optional private key.</param>
         protected RSAlgorithm(X509Certificate2 cert)
+            : base(cert)
         {
-            _publicKey = GetPublicKey(cert) ?? throw new Exception("Certificate's PublicKey cannot be null.");
-            _privateKey = GetPrivateKey(cert);
         }
 
-        /// <inheritdoc />
-        public abstract string Name { get; }
+        protected override RSA GetPublicKey(X509Certificate2 cert) =>
+#if NETSTANDARD || NETCOREAPP || NET462
+            cert.GetRSAPublicKey();
+#else
+            (RSA)cert.PublicKey.Key;
+#endif
 
-        /// <inheritdoc />
-        public abstract HashAlgorithmName HashAlgorithmName { get; }
+        protected override RSA GetPrivateKey(X509Certificate2 cert) =>
+#if NETSTANDARD || NETCOREAPP || NET462
+            cert.GetRSAPrivateKey();
+#else
+            (RSA)cert.PrivateKey;
+#endif
 
-        /// <inheritdoc />
-        public byte[] Sign(byte[] key, byte[] bytesToSign)
-        {
-            if (_privateKey is null)
-                throw new InvalidOperationException("Can't sign data without private key");
-
-            return Sign(bytesToSign);
-        }
-
-        /// <summary>
-        /// Signs the provided bytes.
-        /// </summary>
-        /// <param name="bytesToSign">The bytes to sign.</param>
-        /// <returns>The signed bytes.</returns>
-        public byte[] Sign(byte[] bytesToSign) =>
+        protected override byte[] SignData(byte[] bytesToSign) =>
 #if NET35 || NET40
             ((RSACryptoServiceProvider)_privateKey).SignData(bytesToSign, this.HashAlgorithmName.Name);
 #else
             _privateKey.SignData(bytesToSign, this.HashAlgorithmName, RSASignaturePadding.Pkcs1);
 #endif
 
-        /// <inheritdoc />
-        public bool Verify(byte[] bytesToSign, byte[] signature) =>
+        protected override bool VerifyData(byte[] bytesToSign, byte[] signature) =>
 #if NET35 || NET40
             ((RSACryptoServiceProvider)_publicKey).VerifyData(bytesToSign, this.HashAlgorithmName.Name, signature);
 #else
             _publicKey.VerifyData(bytesToSign, signature, this.HashAlgorithmName, RSASignaturePadding.Pkcs1);
 #endif
-
-        private static RSA GetPrivateKey(X509Certificate2 cert)
-        {
-            if (cert is null)
-                throw new ArgumentNullException(nameof(cert));
-
-#if NETSTANDARD || NETCOREAPP || NET462
-            return cert.GetRSAPrivateKey();
-#else
-            return (RSA)cert.PrivateKey;
-#endif
-        }
-
-        private static RSA GetPublicKey(X509Certificate2 cert)
-        {
-            if (cert is null)
-                throw new ArgumentNullException(nameof(cert));
-
-#if NETSTANDARD || NETCOREAPP || NET462
-            return cert.GetRSAPublicKey();
-#else
-            return (RSA)cert.PublicKey.Key;
-#endif
-        }
     }
 }
