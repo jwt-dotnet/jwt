73fe125c2aa4f10c769a9b0e0f7b31a9940a7754
Alexander Batishchev <abatishchev@gmail.com>
2022-08-25 19:12:51 -0700

    Update JwtEncoderTests.cs

--- tests/JWT.Tests.Common/JwtEncoderTests.cs
+++ tests/JWT.Tests.Common/JwtEncoderTests.cs
@@ -108,7 +108,7 @@ namespace JWT.Tests
             var actual = encoder.Encode(toEncode, key);
 
             actual.Should()
-                .Be(expected, "because the same data encoded with the same key must result in the same token");
+                  .Be(expected, "because the same data encoded with the same key must result in the same token");
         }
     }
 }
