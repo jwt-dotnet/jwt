2f51d221f870895ecfe54c283ccb273fe5ecd661
Alexander Batishchev <abatishchev@gmail.com>
2022-09-07 22:11:37 -0700

    Create DelegateJsonSerializerFactory.cs

---
+++ src/JWT/Serializers/DelegateJsonSerializerFactory.cs
@@ -0,0 +1,29 @@
+using System;
+
+﻿namespace JWT.Serializers
+{
+    internal sealed class DefaultJsonSerializerFactory : IJsonSerializerFactory
+    {
+        private readonly Func<IJsonSerializer> _factory;
+
+        public DelegateJsonSerializerFactory(IJsonSerializer jsonSerializer) :
+            this(() => jsonSerializer)
+        {
+            if (jsonSerializer is null)
+                 throw new ArgumentNullException(nameof(factory))
+        }
+
+        public DelegateJsonSerializerFactory(IJsonSerializerFactory factory)
+            this(() => factory.Create())
+        {
+            if (factory is null)
+                 throw new ArgumentNullException(nameof(factory))
+        }
+
+        public DelegateJsonSerializerFactory(Func<IJsonSerializer> factory)
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+
+        public IJsonSerializer CreateSerializer() =>
+            _factory();
+    }
+}
