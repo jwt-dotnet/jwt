25ce0e134a8d3450cf84ab1c8f770e22e39e097e
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2024-02-04 20:23:34 +0200

    add JwtWebKey properies summaries

--- src/JWT/Jwk/JwtWebKey.cs
+++ src/JWT/Jwk/JwtWebKey.cs
@@ -20,48 +20,79 @@ namespace JWT.Jwk
         } 
 #endif
 
+        /// <summary>
+        /// The "kty" parameter which defines key type which is defined by RFC7518 specification.
+        /// Valid values are "EC" (Elliptic Curve), "RSA" and "oct" (octet sequence used to represent symmetric keys)
+        /// </summary>
         [JsonProperty("kty")]
 #if MODERN_DOTNET
         [JsonPropertyName("kty")]
 #endif
         public string KeyType { get; set; }
 
+        /// <summary>
+        /// The "kid" parameter which defines key id
+        /// </summary>
         [JsonProperty("kid")]
 #if MODERN_DOTNET
         [JsonPropertyName("kid")]
 #endif
         public string KeyId { get; set; }
 
+        /// <summary>
+        /// The "n" (modulus) parameter contains the modulus value for the RSA public key. It is represented as a Base64urlUInt-encoded value.
+        /// <see cref="KeyType"/> ("kty") must be "RSA"
+        /// </summary>
         [JsonProperty("n")]
 #if MODERN_DOTNET
         [JsonPropertyName("n")]
 #endif
         public string Modulus { get; set; }
 
+        /// <summary>
+        /// The "e" (exponent) parameter contains the exponent value for the RSA public key. It is represented as a Base64urlUInt-encoded value.
+        /// <see cref="KeyType"/> ("kty") must be "RSA"
+        /// </summary>
         [JsonProperty("e")]
 #if MODERN_DOTNET
         [JsonPropertyName("e")]
 #endif
         public string Exponent { get; set; }
 
+        /// <summary>
+        /// The "crv" (curve) parameter identifies the cryptographic curve used with the key. RFC7518 defines the following valid values:
+        /// "P-256", "P-384", "P-521" <see cref="KeyType"/> ("kty") must be "EC"
+        /// </summary>
         [JsonProperty("crv")]
 #if MODERN_DOTNET
         [JsonPropertyName("crv")]
 #endif
         public string EllipticCurveType { get; set; }
 
+        /// <summary>
+        /// The "x" (x coordinate) parameter contains the x coordinate for the Elliptic Curve point.  It is represented as the base64url encoding of
+        /// the octet string representation of the coordinate. <see cref="KeyType"/> ("kty") must be "EC"
+        /// </summary>
         [JsonProperty("x")]
 #if MODERN_DOTNET
         [JsonPropertyName("x")]
 #endif
         public string EllipticCurveX { get; set; }
 
+        /// <summary>
+        /// The "y" (y coordinate) parameter contains the y coordinate for the Elliptic Curve point.  It is represented as the base64url encoding of
+        /// the octet string representation of the coordinate. <see cref="KeyType"/> ("kty") must be "EC"
+        /// </summary>
         [JsonProperty("y")]
 #if MODERN_DOTNET
         [JsonPropertyName("y")]
 #endif
         public string EllipticCurveY { get; set; }
 
+        /// <summary>
+        /// The "k" (key value) parameter contains the value of the symmetric (or other single-valued) key.  It is represented as the base64url
+        /// encoding of the octet sequence containing the key value. <see cref="KeyType"/> ("kty") must be "oct"
+        /// </summary>
         [JsonProperty("k")]
 #if MODERN_DOTNET
         [JsonPropertyName("k")]
