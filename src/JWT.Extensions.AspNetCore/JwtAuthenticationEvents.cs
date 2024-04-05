ce3d7298e5641e45d70c1b010b17cc13433e40a8
Dru Sellers <dru@drusellers.com>
2024-04-05 16:46:58 -0500

    PR feedback

--- src/JWT.Extensions.AspNetCore/JwtAuthenticationEvents.cs
+++ src/JWT.Extensions.AspNetCore/JwtAuthenticationEvents.cs
@@ -1,99 +1,101 @@
-namespace JWT.Extensions.AspNetCore;
-
-using System;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.Extensions.Logging;
-using Microsoft.Net.Http.Headers;
-
-public class JwtAuthenticationEvents
+namespace JWT.Extensions.AspNetCore
 {
-    private static readonly Action<ILogger, Exception> _logMissingHeader;
-    private static readonly Action<ILogger, string, string, Exception> _logIncorrectScheme;
-    private static readonly Action<ILogger, Exception> _logEmptyHeader;
-    private static readonly Action<ILogger, Exception> _logSuccessfulTicket;
-    private static readonly Action<ILogger, string, Exception> _logFailedTicket;
 
-    static JwtAuthenticationEvents()
+    using System;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Net.Http.Headers;
+
+    public class JwtAuthenticationEvents
     {
-        _logMissingHeader = LoggerMessage.Define(
-            LogLevel.Information,
-            10,
-            $"Header {nameof(HeaderNames.Authorization)} is empty, returning none");
+        private static readonly Action<ILogger, Exception> _logMissingHeader;
+        private static readonly Action<ILogger, string, string, Exception> _logIncorrectScheme;
+        private static readonly Action<ILogger, Exception> _logEmptyHeader;
+        private static readonly Action<ILogger, Exception> _logSuccessfulTicket;
+        private static readonly Action<ILogger, string, Exception> _logFailedTicket;
 
-        _logIncorrectScheme = LoggerMessage.Define<string, string>(
-            LogLevel.Information,
-            11,
-            $"Header {nameof(HeaderNames.Authorization)} scheme is {{0}}, expected {{1}}, returning none");
+        static JwtAuthenticationEvents()
+        {
+            _logMissingHeader = LoggerMessage.Define(
+                LogLevel.Information,
+                10,
+                $"Header {nameof(HeaderNames.Authorization)} is empty, returning none");
 
-        _logEmptyHeader = LoggerMessage.Define(
-            LogLevel.Information,
-            12,
-            $"Token in header {nameof(HeaderNames.Authorization)} is empty, returning none");
+            _logIncorrectScheme = LoggerMessage.Define<string, string>(
+                LogLevel.Information,
+                11,
+                $"Header {nameof(HeaderNames.Authorization)} scheme is {{ActualScheme}}, expected {{ExpectedScheme}}, returning none");
 
-        _logSuccessfulTicket = LoggerMessage.Define(
-            LogLevel.Information,
-            1,
-            "Successfully decoded JWT, returning success");
+            _logEmptyHeader = LoggerMessage.Define(
+                LogLevel.Information,
+                12,
+                $"Token in header {nameof(HeaderNames.Authorization)} is empty, returning none");
 
-        _logFailedTicket = LoggerMessage.Define<string>(
-            LogLevel.Information,
-            2,
-            "Error decoding JWT: {0}, returning failure");
-    }
+            _logSuccessfulTicket = LoggerMessage.Define(
+                LogLevel.Information,
+                1,
+                "Successfully decoded JWT, returning success");
 
-    public static AuthenticateResult OnMissingHeader(ILogger logger)
-    {
-        _logMissingHeader(logger, null);
-        return AuthenticateResult.NoResult();
-    }
+            _logFailedTicket = LoggerMessage.Define<string>(
+                LogLevel.Information,
+                2,
+                "Error decoding JWT: {0}, returning failure");
+        }
 
-    public static AuthenticateResult OnIncorrectScheme(ILogger logger, string actualScheme, string expectedScheme)
-    {
-        _logIncorrectScheme(logger, actualScheme, expectedScheme, null);
-        return AuthenticateResult.NoResult();
-    }
+        public static AuthenticateResult OnMissingHeader(ILogger logger)
+        {
+            _logMissingHeader(logger, null);
+            return AuthenticateResult.NoResult();
+        }
 
-    public static AuthenticateResult OnEmptyHeader(ILogger logger, string header)
-    {
-        _logEmptyHeader(logger, null);
-        return AuthenticateResult.NoResult();
-    }
+        public static AuthenticateResult OnIncorrectScheme(ILogger logger, string actualScheme, string expectedScheme)
+        {
+            _logIncorrectScheme(logger, actualScheme, expectedScheme, null);
+            return AuthenticateResult.NoResult();
+        }
 
-    public static AuthenticateResult OnSuccessfulTicket(ILogger logger, AuthenticationTicket ticket)
-    {
-        _logSuccessfulTicket(logger, null);
-        return AuthenticateResult.Success(ticket);
-    }
+        public static AuthenticateResult OnEmptyHeader(ILogger logger, string header)
+        {
+            _logEmptyHeader(logger, null);
+            return AuthenticateResult.NoResult();
+        }
 
-    public static AuthenticateResult OnFailedTicket(ILogger logger, Exception ex)
-    {
-        _logFailedTicket(logger, ex.Message, ex);
-        return AuthenticateResult.Fail(ex);
-    }
+        public static AuthenticateResult OnSuccessfulTicket(ILogger logger, AuthenticationTicket ticket)
+        {
+            _logSuccessfulTicket(logger, null);
+            return AuthenticateResult.Success(ticket);
+        }
 
-    public virtual AuthenticateResult SuccessfulTicket(ILogger logger, AuthenticationTicket ticket)
-    {
-        return OnSuccessfulTicket(logger, ticket);
-    }
+        public static AuthenticateResult OnFailedTicket(ILogger logger, Exception ex)
+        {
+            _logFailedTicket(logger, ex.Message, ex);
+            return AuthenticateResult.Fail(ex);
+        }
 
-    public virtual AuthenticateResult FailedTicket(ILogger logger, Exception exception)
-    {
-        return OnFailedTicket(logger, exception);
-    }
+        public virtual AuthenticateResult SuccessfulTicket(ILogger logger, AuthenticationTicket ticket)
+        {
+            return OnSuccessfulTicket(logger, ticket);
+        }
 
-    public virtual AuthenticateResult EmptyHeader(ILogger logger, string header)
-    {
-        return OnEmptyHeader(logger, header);
+        public virtual AuthenticateResult FailedTicket(ILogger logger, Exception exception)
+        {
+            return OnFailedTicket(logger, exception);
+        }
 
-    }
+        public virtual AuthenticateResult EmptyHeader(ILogger logger, string header)
+        {
+            return OnEmptyHeader(logger, header);
 
-    public virtual AuthenticateResult IncorrectScheme(ILogger logger, string actualScheme, string expectedScheme)
-    {
-        return OnIncorrectScheme(logger, actualScheme, expectedScheme);
-    }
+        }
 
-    public virtual AuthenticateResult MissingHeader(ILogger logger)
-    {
-        return OnMissingHeader(logger);
+        public virtual AuthenticateResult IncorrectScheme(ILogger logger, string actualScheme, string expectedScheme)
+        {
+            return OnIncorrectScheme(logger, actualScheme, expectedScheme);
+        }
+
+        public virtual AuthenticateResult MissingHeader(ILogger logger)
+        {
+            return OnMissingHeader(logger);
+        }
     }
 }
